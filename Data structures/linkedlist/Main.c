634224e3057f44be76b51924a1bc2d491c0dda43
junrdev <juniorprogrammer09@gmail.com>
2023-03-24 11:25:47 +0300

    more linked list

--- Data structures/linkedlist/Main.c
+++ Data structures/linkedlist/Main.c
@@ -32,10 +32,39 @@ int main(){
     //node * hd = create_node(10);
     insert(list1, 10);
     insert(list1, 20);
+    insert(list1, 40);
     printf("Size of list is %d\n", getsize(list1));
     print_list(list1);
 
+    //printf("\nNode 0 at position %d\n", search_node(list1,0));
+
     remove_node(list1, 20);
+
+    print_list(list1);
+
+    remove_front(list1);
+
+    print_list(list1);
+
+    remove_front(list1);
+
+    print_list(list1);
+
+    remove_node(list1,40);
+
+    print_list(list1);
+
+    printf("List capacity at %d\n", getsize(list1));
+
+    list * list2 = list_init(0);
+    insert(list2,10);
+    insert(list2,20);
+    insert(list2,30);
+    printf("List capacity at %d\n", getsize(list2));
+    print_list(list2);
+
+    insert_at(list2, 99, 3);
+    print_list(list2);
    
     return 0;
 }
--- Data structures/linkedlist/linkedlist.h
+++ Data structures/linkedlist/linkedlist.h
@@ -6,6 +6,7 @@ typedef struct {
     int size;
 }list;
 
+int static_node_position=0;
 
 list * list_init(int root_data){
 
@@ -17,6 +18,9 @@ list * list_init(int root_data){
     return new_list;
 }
 
+/**
+    inserts a node at the end of the list
+*/
 void insert(list * lst, int data){
 
     node * temp = lst->head;
@@ -31,44 +35,132 @@ void insert(list * lst, int data){
     lst->size ++;
 }
 
+/**
+    returns the number of nodes in the list
+*/
 int getsize(list * lst){
     return lst->size;
 }
 
+/**
+    prints the entire list
+*/
 void print_list(list * lst){
-    node * temphead = lst->head;
 
+    if(lst == NULL || lst -> head == NULL){
+        printf("\nList is empty\n");
+        return;
+    }
+    node * temphead = lst->head;
+    printf("List @%p{ ", lst);
     while (temphead != NULL){
         printf("Node[@%p]:%d -> ", temphead, temphead->element);
         temphead=temphead->next;
     }
+
+    printf(" }\n");
 }
 
+/**
+    check if a list contains an element
+*/
 bool contains(list * lst, int el){
 
     if(lst == NULL)
         return false;
     node * tnode = lst->head;
-
+    int i=1;
     while(tnode != NULL){ 
-        if(tnode->element = el)
+        if(tnode->element == el){
+            static_node_position=i;
             return true;
+        }
+
         tnode=tnode->next;
+        i++;
     }
     return false;
 }
 
+/**
+    uses the contains method internally to retrieve the position of a node in a list
+    makes operations such as deletion very easy
+*/
+int search_node(list * lst, int data){
+    if(contains(lst,data))
+        return static_node_position;
+    return -1;
+}
+
+/**
+    to remove the element at the front of the list
+*/
+void remove_front(list * lst){
+    //printf("\nList size is %d\t", getsize(lst));
+    if(lst == NULL)
+        return;
+    else if(lst-> size ==1){
+        lst->head =NULL;
+        lst->size --;
+    }
+    else{
+        //printf("In here\n");
+        node * tnode = lst->head;
+        node * new_head = tnode->next;
+        tnode == NULL;
+        lst->head = new_head;
+        lst->size --;
+    }
+}
+
+
 void remove_node(list * lst, int data){
 
         if(!contains(lst,data)){
-            printf("The element %d is not available in the list\n", data);
+            printf("\nThe element %d is not available in the list\n", data);
             return;
         }
 
-        node * temphead = lst->head;
-        node * prevnode;
-
-        while(temphead != NULL){
-           
+        if(lst ->head -> element == data){//if we 
+            remove_front(lst);
+            return;
         }
+        node * temphead = lst->head;
+        node * prevnode=temphead;
+
+        int counter=1;
+
+        while(counter < search_node(lst,data)){
+            prevnode=temphead;//finally points to the node before the one to be deleted
+            temphead=temphead->next;//finally points to the node to be deleted
+            counter++;
+        }
+
+        prevnode->next = temphead->next;
+        lst->size --;
+        temphead = NULL;
+}
+
+void insert_at(list * lst, int data, int position){
+
+    if(position > getsize(lst)){
+        printf("The position is greater than the current size.");
+        printf("Inserting at the end");
+        insert(lst, data);
+        return;
+    }
+
+    int counter =1;
+    node * temphead=lst->head;
+
+    while(counter < getsize(lst) && counter < position){
+        temphead = temphead->next;
+        counter ++;
+    }
+
+    node * new_node = create_node(data);
+
+    new_node->next = temphead->next;
+    temphead->next = new_node;
+    lst->size ++;
 }
