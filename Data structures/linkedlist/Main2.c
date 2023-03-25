f5a641e7c69acd719dc3cfded860f48bae9ed428
junrdev <juniorprogrammer09@gmail.com>
2023-03-25 20:28:04 +0300

    doubly linked lists

--- Data structures/linkedlist/Main2.c
+++ Data structures/linkedlist/Main2.c
@@ -1,5 +1,7 @@
 /**
  * 
+ *  This file contains the code related to doubly.h which represents a doubly linked list
+ * 
 */
 #include<stdio.h>
 #include "doubly.h"
@@ -8,5 +10,28 @@ int main(){
     
     doubly_node * node1 = create_doublyNode(10);
     print_node(node1);
+
+    doubly_list * list1 = list_init(20);
+    insert_node(list1,30);
+    insert_node(list1,40);
+    insert_node(list1,50);
+    insert_node(list1,60);
+
+    print_list(list1);
+
+    printf("\n");
+    delete_node(list1,50);
+    print_list(list1);
+    printf("\n");
+    insert_at(list1,3,99);
+    printf("\n");
+    print_list(list1);
+
+    printf("\n");
+    insert_at(list1,5,909);
+    printf("\n");
+    print_list(list1);
+
+
     return 0;
 }
