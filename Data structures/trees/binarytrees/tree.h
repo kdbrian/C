ab1c9a932256fd10ced4380d065b902b5766e95a
junrdev <juniorprogrammer09@gmail.com>
2023-03-28 08:05:33 +0300

    node insertion functions impl

--- Data structures/trees/binarytrees/tree.h
+++ Data structures/trees/binarytrees/tree.h
@@ -90,9 +90,58 @@ void printPreOrder(node * root){
     if(root == NULL)
         return;
     else{
-        printf("Node[%d]\n", root->data);
+        printf("Node[%d]@[%p] {left@%p, right@%p}\n", root->data, root, root->left_child, root->right_child);
         printPreOrder(root->left_child);
         printPreOrder(root->right_child);
     }
 }
 
+/**
+ * Insert the node at a position in the list
+ * 
+ * does this by traversing the list inorder and start insertion from the left most
+ * 
+*/
+void insert(tree * tr, int data){
+
+
+}
+
+/**
+ * find a suitable position to insert a node
+*/
+node * findPosition(tree * tr){
+
+    node * temp = tr->root_node;
+
+    if(temp->left_child == NULL || temp->right_child == NULL)
+        return temp;
+    else if(temp == NULL)
+        return NULL;
+    return findPosition(temp->left_child);
+
+}
+
+void * insertRight(tree * tr, node * n, int data){
+
+    if(n->data != 0)
+        return;
+    
+    node * new_node = createNode(data);
+
+    n->right_child = new_node;
+
+    tr->size ++;
+
+}
+
+void * insertLeft(tree * tr, node * n, int data){
+
+    if(n->data != 0)
+        return;
+    
+    node * new_node = createNode(data);
+
+    n->right_child = new_node;
+    tr->size ++;
+}
